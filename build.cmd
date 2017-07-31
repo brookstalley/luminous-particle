24dbbfb70c127cf623ab0638e9a4b9824792919c
Brooks Talley <brooks@tangentry.com>
2017-07-31 08:17:41 -0700

    Update build batch files

--- build.cmd
+++ build.cmd
@@ -1,4 +1,7 @@
-clear
+set device=light1
+if not "%~1"=="" (
+  SET device=%1
+)
 cd \particle\firmware\main
-make all PLATFORM=photon APPDIR=../../luminous-particle && particle flash light1 \particle\firmware\modules\luminous-particle\target\luminous-particle.bin
+make all PLATFORM=photon APPDIR=../../luminous-particle && particle flash %device% \particle\firmware\modules\luminous-particle\target\luminous-particle.bin
 cd \particle\luminous-particle
