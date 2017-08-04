110d6f019018c5fd64caff3d51c9b3f3e62dcd39
Brooks Talley <brooks@tangentry.com>
2017-08-04 09:24:47 -0700

    Starting wifi configurability

---
+++ src/credentials.h
@@ -0,0 +1,10 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Installation-specific credentials
+// Should not be checked in to git
+
+const String wifiNetwork  = "your_network";
+const String wifiPassword = "your_password";
+
+#endif /* ifndef CREDENTIALS_H */
