fa1939e61b87ddf9c2b12fd2c0ed26d659fd8353
Brooks Talley <brooks@tangentry.com>
2017-08-04 09:40:16 -0700

    Clean credentials

---
+++ src/credentials.h
@@ -0,0 +1,10 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Installation-specific credentials
+// Should not be checked in to git
+
+const String wifiNetwork  = "NETWORK";
+const String wifiPassword = "PASSWORD";
+
+#endif /* ifndef CREDENTIALS_H */
