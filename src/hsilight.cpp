50e03d9ee174d7ddf3434d0fe074a014059c5ed7
Brooks Talley <brooks@tangentry.com>
2017-08-10 07:53:17 -0700

    Fix duplicate color skipping

--- src/hsilight.cpp
+++ src/hsilight.cpp
@@ -1,4 +1,5 @@
-//*********************************************************
+// *********************************************************
+
 //
 // Luminous
 // Copyright 2017 Brooks Talley
@@ -18,7 +19,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 //
-//**********************************************************
+// **********************************************************
 
 #include "hsilight.h"
 
@@ -32,130 +33,149 @@
 
 // Constructor with no temperature sensor; set the interface to nullptr,
 // and adddress to 0
-HSILight::HSILight(const char *name, const CompositeModule& compositeModule,
-                   std::shared_ptr<OutputInterface>outputInterface, const uint16_t outputLocalAddress,
-                   const std::shared_ptr<E131>e131, const uint16_t e131LocalAddress,
+HSILight::HSILight(const char                          *name,
+                   const CompositeModule              & compositeModule,
+                   std::shared_ptr<OutputInterface>     outputInterface,
+                   const uint16_t                       outputLocalAddress,
+                   const std::shared_ptr<E131>          e131,
+                   const uint16_t                       e131LocalAddress,
                    std::shared_ptr<TemperatureInterface>temperatureInterface,
-                   const uint16_t temperatureLocalAddress
+                   const uint16_t                       temperatureLocalAddress
                    ) :
-	_name(name),
-	_compositeModule(compositeModule),
-	_outputInterface(outputInterface),
-	_outputLocalAddress(outputLocalAddress),
-	_e131(e131),
-	_e131LocalAddress(e131LocalAddress),
-	_temperatureInterface(temperatureInterface),
-	_temperatureLocalAddress(temperatureLocalAddress),
-	_localBrightness(1.0f),
-	_temperature(-1.0f),
-	_diagnostic("")
-{
-}
+  _name(name),
+  _compositeModule(compositeModule),
+  _outputInterface(outputInterface),
+  _outputLocalAddress(outputLocalAddress),
+  _e131(e131),
+  _e131LocalAddress(e131LocalAddress),
+  _temperatureInterface(temperatureInterface),
+  _temperatureLocalAddress(temperatureLocalAddress),
+  _localBrightness(1.0f),
+  _temperature(-1.0f),
+  _diagnostic("")
+{}
 
 // Constructor with no temperature sensor; set the interface to nullptr,
 // and adddress to 0
-HSILight::HSILight(const char *name, const CompositeModule& compositeModule,
-                   std::shared_ptr<OutputInterface>outputInterface, const uint16_t outputLocalAddress,
-                   const std::shared_ptr<E131>e131, const uint16_t e131LocalAddress
+HSILight::HSILight(const char                     *name,
+                   const CompositeModule         & compositeModule,
+                   std::shared_ptr<OutputInterface>outputInterface,
+                   const uint16_t                  outputLocalAddress,
+                   const std::shared_ptr<E131>     e131,
+                   const uint16_t                  e131LocalAddress
                    ) :
-	_name(name),
-	_compositeModule(compositeModule),
-	_outputInterface(outputInterface),
-	_outputLocalAddress(outputLocalAddress),
-	_e131(e131),
-	_e131LocalAddress(e131LocalAddress),
-	_temperatureInterface(nullptr),
-	_temperatureLocalAddress(0),
-	_localBrightness(1.0f),
-	_temperature(-1.0f),
-	_diagnostic("")
-{
-}
+  _name(name),
+  _compositeModule(compositeModule),
+  _outputInterface(outputInterface),
+  _outputLocalAddress(outputLocalAddress),
+  _e131(e131),
+  _e131LocalAddress(e131LocalAddress),
+  _temperatureInterface(nullptr),
+  _temperatureLocalAddress(0),
+  _localBrightness(1.0f),
+  _temperature(-1.0f),
+  _diagnostic("")
+{}
 
 void HSILight::begin() {
-	_lastColor = HSIColor(0, 0,0);
-	_emitterPowers = _compositeModule.emitterPowersFromHSI(_lastColor);
+  _lastColor     = HSIColor(0, 0, 0);
+  _emitterPowers = _compositeModule.emitterPowersFromHSI(_lastColor);
 
-	_lastE131PacketCount = 0;
-	debugPrintf(DEBUG_TRACE, "HSILight: begin (%u)", _emitterPowers.size());
+  _lastE131PacketCount = 0;
+  debugPrintf(DEBUG_TRACE, "HSILight: begin (%u)", _emitterPowers.size());
 
-	for (unsigned int i = 0; i < _emitterPowers.size(); i++) {
-		debugPrintf(DEBUG_INSANE, "HSILight: emitter %u la %u", i, _emitterPowers[i].outputLocalAddress);
-	}
+  for (unsigned int i = 0; i < _emitterPowers.size(); i++) {
+    debugPrintf(DEBUG_INSANE,
+                "HSILight: emitter %u la %u",
+                i,
+                _emitterPowers[i].outputLocalAddress);
+  }
 }
 
 void HSILight::setColor(const HSIColor& color)  {
-	// No sense doing it again for the same value
-	if (color == _lastColor)
-		return;
+  // No sense doing it again for the same value
+  if (color == _lastColor) {
+    debugPrint(DEBUG_INSANE,
+               "HSILight::setColor skipping duplicate");
+    return;
+  }
+  _lastColor     = color;
+  _emitterPowers = _compositeModule.emitterPowersFromHSI(color);
 
-	_emitterPowers = _compositeModule.emitterPowersFromHSI(color);
-	/*
-	      for (unsigned int i = 0; i < _emitterPowers.size(); i++) {
-	              debugPrintf(DEBUG_INSANE, "HSILight: emitter %u power %f", i, _emitterPowers[i].power);
-	      }
-	 */
-	setEmitters();
+  /*
+        for (unsigned int i = 0; i < _emitterPowers.size(); i++) {
+                debugPrintf(DEBUG_INSANE, "HSILight: emitter %u power %f", i,
+                   _emitterPowers[i].power);
+        }
+   */
+  setEmitters();
 }
 
 void HSILight::setSingleEmitterOn(unsigned int index) {
-	debugPrintf(DEBUG_INSANE, "HSILight: Setting single emitter for index %u (%u)", index, _emitterPowers.size());
+  debugPrintf(DEBUG_INSANE,
+              "HSILight: Setting single emitter for index %u (%u)",
+              index,
+              _emitterPowers.size());
 
-	float thisEmitter = 0.0f;
+  float thisEmitter = 0.0f;
 
-	for (unsigned int i = 0; i < _emitterPowers.size(); i++) {
-		thisEmitter = (index % _emitterPowers.size() == i) ? 1.0 : 0.0;
-		debugPrintf(DEBUG_INSANE, "HSILight: emitter %u power %f", i, thisEmitter);
+  for (unsigned int i = 0; i < _emitterPowers.size(); i++) {
+    thisEmitter = (index % _emitterPowers.size() == i) ? 1.0 : 0.0;
+    debugPrintf(DEBUG_INSANE, "HSILight: emitter %u power %f", i, thisEmitter);
 
-		_emitterPowers[i].power = thisEmitter;
-	}
-	setEmitters();
+    _emitterPowers[i].power = thisEmitter;
+  }
+  setEmitters();
 }
 
 void HSILight::setEmitters() {
-	debugPrint(DEBUG_INSANE, "HSILight::setEmitters start");
-	_diagnostic[0] = '\0';
-	for (auto const &e : _emitterPowers) {
-		sprintf(_diagnostic + strlen(_diagnostic), "%02X ", (uint16_t)round(e.power * 255));
-	}
-	float scaleFactor = globalBrightness * _localBrightness;
-	_outputInterface->setEmitterPowers(_emitterPowers, scaleFactor);
+  debugPrint(DEBUG_INSANE, "HSILight::setEmitters start");
+  _diagnostic[0] = '\0';
+
+  for (auto const& e : _emitterPowers) {
+    sprintf(_diagnostic + strlen(_diagnostic), "%02X ",
+            (uint16_t)round(e.power * 255));
+  }
+  float scaleFactor = globalBrightness * _localBrightness;
+  _outputInterface->setEmitterPowers(_emitterPowers, scaleFactor);
 }
 
 const char * HSILight::getName(void) const {
-	return _name;
+  return _name;
 }
 
 const char * HSILight::getDiagnostic(void) const {
-	return _diagnostic;
+  return _diagnostic;
 }
 
-std::shared_ptr<E131> HSILight::getE131() const {
-	return _e131;
+std::shared_ptr<E131>HSILight::getE131() const {
+  return _e131;
 }
 
 const uint16_t HSILight::getE131LocalAddress() const {
-	return _e131LocalAddress;
+  return _e131LocalAddress;
 }
 
-void HSILight::setTemperatureInterface(std::shared_ptr<TemperatureInterface>temperatureInterface) {
-	_temperatureInterface = temperatureInterface;
+void HSILight::setTemperatureInterface(
+  std::shared_ptr<TemperatureInterface>temperatureInterface) {
+  _temperatureInterface = temperatureInterface;
 }
 
 float HSILight::updateTemperature(void) {
-	if (_temperatureInterface == NULL) return -1.0f;
-	_temperature = _temperatureInterface->getTemperature(_temperatureLocalAddress);
-	return _temperature;
+  if (_temperatureInterface == NULL) return -1.0f;
+
+  _temperature = _temperatureInterface->getTemperature(_temperatureLocalAddress);
+  return _temperature;
 }
 
 float HSILight::getTemperature(void) {
-	if (_temperatureInterface != NULL) {
-		if ((millis() - _tempertureUpdatedMillis) < TEMPERATURE_VALID_MILLIS) {
-			return _temperature;
-		} else {
-			return updateTemperature();
-		}
-	} else {
-		return -1.0f;
-	}
+  if (_temperatureInterface != NULL) {
+    if ((millis() - _tempertureUpdatedMillis) < TEMPERATURE_VALID_MILLIS) {
+      return _temperature;
+    } else {
+      return updateTemperature();
+    }
+  } else {
+    return -1.0f;
+  }
 }
