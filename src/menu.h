2b29298104c34dfdd4a682050cf7b9eff84273e5
Brooks Talley <brooks@tangentry.com>
2017-08-15 07:22:23 -0700

    Add menu files

---
+++ src/menu.h
@@ -0,0 +1,36 @@
+// *********************************************************
+
+//
+// Luminous
+// Copyright 2017 Brooks Talley
+//
+// Portions derived from TeensyLED, copyright 2015 Brian Neltner
+//
+// Luminous is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Luminous is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Luminous.  If not, see <http://www.gnu.org/licenses/>.
+//
+// **********************************************************
+
+#ifndef LUMINOUSMENU_H
+# define LUMINOUSMENU_H
+
+
+#endif // ifndef LUMINOUSMENU_H
+
+class Menu {
+private:
+
+  Menu _parentMenu;
+
+public:
+}
