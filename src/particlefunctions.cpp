06521e66f57dee4cca7ac25d539dcc722596f4c6
Brooks Talley <brooks@tangentry.com>
2017-09-02 18:11:33 -0700

    Automatic wifi reconnection

--- src/particlefunctions.cpp
+++ src/particlefunctions.cpp
@@ -1,4 +1,5 @@
-//*********************************************************
+// *********************************************************
+
 //
 // Luminous
 // Copyright 2017 Brooks Talley
@@ -18,7 +19,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Luminous.  If not, see <http://www.gnu.org/licenses/>.
 //
-//**********************************************************
+// **********************************************************
 #include "particlefunctions.h"
 #include "luminous-particle.h"
 
@@ -33,121 +34,122 @@ particleState particleCurrentState = PARTICLE_DISCONNECTED;
 particleState particleDesiredState = PARTICLE_DISCONNECTED;
 
 wifiState wifiCurrentState = WIFI_DISCONNECTED;
+wifiState wifiDesiredState = WIFI_DISCONNECTED;
 
 LEDStatus luminousBooting(RGB_COLOR_BLUE, LED_PATTERN_BLINK, LED_SPEED_FAST, LED_PRIORITY_IMPORTANT);
 
 void particleDisconnect() {
-	debugPrint(DEBUG_TRACE, "particleDisconnect: start");;
+  debugPrint(DEBUG_TRACE, "particleDisconnect: start");
 
-	if (!Particle.connected() && (particleDesiredState == PARTICLE_DISCONNECTED)) {
-		// We're already not connected, and we want to be disconnected.
-		particleCurrentState = PARTICLE_DISCONNECTED;
-	}
+  if (!Particle.connected() && (particleDesiredState == PARTICLE_DISCONNECTED)) {
+    // We're already not connected, and we want to be disconnected.
+    particleCurrentState = PARTICLE_DISCONNECTED;
+  }
 
-	if ((particleCurrentState == PARTICLE_CONNECTED) || (particleDesiredState == PARTICLE_CONNECTED)) {
-		// We are either connected or in the process of connecting. Shut that down.
-		debugPrint(DEBUG_MANDATORY, "particleDisconnect: Disconnecting from Particle cloud");
-		Particle.disconnect();
-		particleDesiredState = PARTICLE_DISCONNECTED;
-		debugPrint(DEBUG_TRACE, "particleDisconnect: disconnecting");;
-		return;
-	}
-	debugPrint(DEBUG_TRACE, "particleDisconnect: end");;
+  if ((particleCurrentState == PARTICLE_CONNECTED) || (particleDesiredState == PARTICLE_CONNECTED)) {
+    // We are either connected or in the process of connecting. Shut that down.
+    debugPrint(DEBUG_MANDATORY, "particleDisconnect: Disconnecting from Particle cloud");
+    Particle.disconnect();
+    particleDesiredState = PARTICLE_DISCONNECTED;
+    debugPrint(DEBUG_TRACE, "particleDisconnect: disconnecting");
+    return;
+  }
+  debugPrint(DEBUG_TRACE, "particleDisconnect: end");
 }
 
 void particleConnect() {
-	static unsigned long connectionStartMillis = 0;
+  static unsigned long connectionStartMillis = 0;
 
-	if (Particle.connected()) {
-		// We're already connected, thanks, but maybe the state wasn't updated
-		particleConnectionStarted();
-		return;
-	}
+  if (Particle.connected()) {
+    // We're already connected, thanks, but maybe the state wasn't updated
+    particleConnectionStarted();
+    return;
+  }
 
-	// We are not connected, but the desired state is to be connected
-	if (particleDesiredState == PARTICLE_CONNECTED) {
-		if ((millis() - connectionStartMillis) > PARTICLE_CONNECTION_TIMEOUT) {
-			// Timeout. Give up
-			// TODO: build periodic reconnection attempt
-			debugPrint(DEBUG_TRACE, "Timed out connecting to Particle cloud");;
-			particleDisconnect();
-		}
+  // We are not connected, but the desired state is to be connected
+  if (particleDesiredState == PARTICLE_CONNECTED) {
+    if ((millis() - connectionStartMillis) > PARTICLE_CONNECTION_TIMEOUT) {
+      // Timeout. Give up
+      // TODO: build periodic reconnection attempt
+      debugPrint(DEBUG_TRACE, "Timed out connecting to Particle cloud");
+      particleDisconnect();
+    }
 
-		// Have not connected yet, but still within timeout
-		return;
-	}
+    // Have not connected yet, but still within timeout
+    return;
+  }
 
-	// We must be in PARTICLE_DISCONNECTED state, but someone called Connect()
-	connectionStartMillis = millis();
-	Particle.connect();
-	particleDesiredState = PARTICLE_CONNECTED;
-	debugPrint(DEBUG_MANDATORY, "particleConnect: Connecting to Particle cloud");;
+  // We must be in PARTICLE_DISCONNECTED state, but someone called Connect()
+  connectionStartMillis = millis();
+  Particle.connect();
+  particleDesiredState = PARTICLE_CONNECTED;
+  debugPrint(DEBUG_MANDATORY, "particleConnect: Connecting to Particle cloud");
 }
 
 void particleToggle() {
-	if (particleDesiredState == PARTICLE_CONNECTED) {
-		debugPrint(DEBUG_TRACE, "particleToggle: disconnecting");;
-		particleDisconnect();
-	} else {
-		debugPrint(DEBUG_TRACE, "particleToggle: connecting");;
-		particleConnect();
-	}
+  if (particleDesiredState == PARTICLE_CONNECTED) {
+    debugPrint(DEBUG_TRACE, "particleToggle: disconnecting");
+    particleDisconnect();
+  } else {
+    debugPrint(DEBUG_TRACE, "particleToggle: connecting");
+    particleConnect();
+  }
 }
 
 void particleProcess() {
-	if (Particle.connected()) {
-		// If we don't know we're connected, change that
-		if (particleCurrentState == PARTICLE_DISCONNECTED) {
-			debugPrint(DEBUG_MANDATORY, "particleProcess: Particle connection established");
-			particleConnectionStarted();
-		}
-	} else {
-		// We are not connected
-		if (particleCurrentState == PARTICLE_CONNECTED) {
-			// We think we're connected but Particle thinks otherwise
-			particleCurrentState = PARTICLE_DISCONNECTED;
+  if (Particle.connected()) {
+    // If we don't know we're connected, change that
+    if (particleCurrentState == PARTICLE_DISCONNECTED) {
+      debugPrint(DEBUG_MANDATORY, "particleProcess: Particle connection established");
+      particleConnectionStarted();
+    }
+  } else {
+    // We are not connected
+    if (particleCurrentState == PARTICLE_CONNECTED) {
+      // We think we're connected but Particle thinks otherwise
+      particleCurrentState = PARTICLE_DISCONNECTED;
 
-			if (particleDesiredState == PARTICLE_CONNECTED) {
-				// ...but we WANT to be connected, so try to reconnect
-				particleConnect();
-			}
-		}
-	}
+      if (particleDesiredState == PARTICLE_CONNECTED) {
+        // ...but we WANT to be connected, so try to reconnect
+        particleConnect();
+      }
+    }
+  }
 
-	// No matter what, run the process
-	Particle.process();
+  // No matter what, run the process
+  Particle.process();
 }
 
 int particleFunctionMode(String command) {
-	debugPrintf(DEBUG_TRACE, "particleFunctionMode: starting for command %s", command);
+  debugPrintf(DEBUG_TRACE, "particleFunctionMode: starting for command %s", command);
 
-	if (setModeByName(command)) {
-		return 1;
-	}
-	return -1;
+  if (setModeByName(command)) {
+    return 1;
+  }
+  return -1;
 }
 
 int particleFunctionBrightness(String command) {
-	debugPrintf(DEBUG_MANDATORY, "particleFunctionBrightness: starting for command %s", command);
-	int brightness = command.toInt();
+  debugPrintf(DEBUG_MANDATORY, "particleFunctionBrightness: starting for command %s", command);
+  int brightness = command.toInt();
 
-	if ((brightness >= 0) && (brightness <= 100)) {
-		globalBrightness     = ((float)brightness / 100.0f);
-		lastBrightnessRemote = true;
-		brightnessControl.setActivityThreshold(8.0f);
-		return 1;
-	}
-	return -1;
+  if ((brightness >= 0) && (brightness <= 100)) {
+    globalBrightness     = ((float)brightness / 100.0f);
+    lastBrightnessRemote = true;
+    brightnessControl.setActivityThreshold(8.0f);
+    return 1;
+  }
+  return -1;
 }
 
 void particleSetupFunctions() {
-	debugPrint(DEBUG_MANDATORY, "particleSetupFunctions: starting");
-	Particle.function("mode",       particleFunctionMode);
-	Particle.function("brigntness", particleFunctionBrightness);
+  debugPrint(DEBUG_MANDATORY, "particleSetupFunctions: starting");
+  Particle.function("mode",       particleFunctionMode);
+  Particle.function("brigntness", particleFunctionBrightness);
 }
 
 void particleConnectionStarted() {
-	debugPrint(DEBUG_MANDATORY, "particleConnectionStarted: Particle connection established");
-	particleCurrentState = PARTICLE_CONNECTED;
-	particleSetupFunctions();
+  debugPrint(DEBUG_MANDATORY, "particleConnectionStarted: Particle connection established");
+  particleCurrentState = PARTICLE_CONNECTED;
+  particleSetupFunctions();
 }
